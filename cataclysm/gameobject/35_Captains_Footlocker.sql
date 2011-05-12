37771ce5d8bb5d3f951cacd869351144c9d151a1
Eugene <codels25@gmail.com>
2011-05-12 19:07:47 +0400

    add quest for gameobject 35

---
+++ cataclysm/gameobject/35_Captains_Footlocker.sql
@@ -0,0 +1,7 @@
+DELETE FROM `gameobject_questrelation` WHERE `id` = 35;
+INSERT INTO `gameobject_questrelation` (`id`, `quest`) VALUES
+(35, 26354);
+
+DELETE FROM `gameobject_involvedrelation` WHERE `id` = 35;
+INSERT INTO `gameobject_involvedrelation` (`id`, `quest`) VALUES
+(35, 26353);
