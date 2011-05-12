5879c6a08dfd4b7d21b07af212b7ea1c46be5218
Eugene <codels25@gmail.com>
2011-05-12 15:50:36 +0400

    fix summon 30564 for ytdb

---
+++ wotlk/creature/30272_Njorndar_Proto-Drake.sql
@@ -0,0 +1,3 @@
+INSERT INTO `npc_spellclick_spells` (`npc_entry`, `spell_id`, `quest_start`, `quest_start_active`,
+`quest_end`, `cast_flags`, `aura_required`, `aura_forbidden`, `user_type`) VALUES
+(30272, 57401, 13071, 1, 13071, 1, 0, 0, 0);
