2044217535b53773e86da08f8b86fdeb209bc0ef
Eugene <codels25@gmail.com>
2011-05-13 16:26:49 +0400

    add quest for gameobject 61

---
+++ cataclysm/gameobject/61_A_Weathered_Grave.sql
@@ -0,0 +1,7 @@
+DELETE FROM `gameobject_questrelation` WHERE `id` = 61;
+INSERT INTO `gameobject_questrelation` (`id`, `quest`) VALUES
+(61, 26793);
+
+DELETE FROM `gameobject_involvedrelation` WHERE `id` = 61;
+INSERT INTO `gameobject_involvedrelation` (`id`, `quest`) VALUES
+(61, 26797);
