39fce53dc79a245a82d4e0abca8abcb04e0031a1
Eugene <codels25@gmail.com>
2011-05-13 16:28:07 +0400

    add quest for gameobjecy 2908

---
+++ cataclysm/gameobject/2908_Sealed_Supply_Crate.sql
@@ -0,0 +1,7 @@
+DELETE FROM `gameobject_questrelation` WHERE `id` = 2908;
+INSERT INTO `gameobject_questrelation` (`id`, `quest`) VALUES
+(2908, 751);
+
+DELETE FROM `gameobject_involvedrelation` WHERE `id` = 2908;
+INSERT INTO `gameobject_involvedrelation` (`id`, `quest`) VALUES
+(2908, 749);
