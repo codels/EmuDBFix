31178cfbd1fe38e98c94bf3663da0ba22812ed7a
Eugene <codels25@gmail.com>
2011-05-13 16:27:21 +0400

    add quest for gameobject 35251

---
+++ cataclysm/gameobject/35251_Karnitols_Chest.sql
@@ -0,0 +1,7 @@
+DELETE FROM `gameobject_questrelation` WHERE `id` = 35251;
+INSERT INTO `gameobject_questrelation` (`id`, `quest`) VALUES
+(35251, 1455);
+
+DELETE FROM `gameobject_involvedrelation` WHERE `id` = 35251;
+INSERT INTO `gameobject_involvedrelation` (`id`, `quest`) VALUES
+(35251, 1454);
