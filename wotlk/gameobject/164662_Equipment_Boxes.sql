2f315c2cf8301f77fbf3d792647fa22b357812fa
Eugene <codels25@gmail.com>
2011-05-16 22:31:29 +0400

    fix drop item 11127 and fix entry gameobject_loot_template for ytdb

---
+++ wotlk/gameobject/164662_Equipment_Boxes.sql
@@ -0,0 +1,7 @@
+UPDATE `gameobject_template` SET
+	`data1` = 10984
+WHERE `entry` = 164662;
+
+DELETE FROM `gameobject_loot_template` WHERE `entry` IN (164662, 10984); -- 164662 ytdb loot
+INSERT INTO `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(10984, 11127, -100, 1, 0, 1, 1);
