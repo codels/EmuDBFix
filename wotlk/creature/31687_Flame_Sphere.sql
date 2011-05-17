8245eba7fad23458482f51087e4b1d4e014726be
Eugene <codels25@gmail.com>
2011-05-17 21:53:48 +0400

    fix visible and level sphere in ahnkahet

---
+++ wotlk/creature/31687_Flame_Sphere.sql
@@ -0,0 +1,8 @@
+UPDATE `creature_template` SET
+        `modelid1` = 26767,
+        `modelid2` = 0,
+        `minlevel` = 80,
+        `maxlevel` = 80,
+        `flags_extra` = 128,
+        `unit_flags` = 0
+WHERE `entry` = 31687;
