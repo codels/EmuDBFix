35fdf8d33f7c7d25d1d0f839c97e22c7e75a4b81
Eugene <codels25@gmail.com>
2011-05-17 13:15:51 +0400

    fix stats for item 44417

---
+++ wotlk/items/44417_Hateful_Gladiators_War_Staff.sql
@@ -0,0 +1,13 @@
+UPDATE `item_template` SET
+        `StatsCount` = 5,
+        `stat_type1` = 7,
+        `stat_value1` = 130,
+        `stat_type2` = 5,
+        `stat_value2` = 57,
+        `stat_type3` = 35,
+        `stat_value3` = 75,
+        `stat_type4` = 36,
+        `stat_value4` = 75,
+        `stat_type5` = 45,
+        `stat_value5` = 408
+WHERE `entry` = 44417;
