2d86390677a88c35720c739c71de488293e8d017
Eugene <codels25@gmail.com>
2011-05-17 21:57:20 +0400

    fix coord teleport from ahnkahet & AzjolNerub

---
+++ wotlk/areatrigger/5113_Azjol-Nerub_exit_back.sql
@@ -0,0 +1,6 @@
+UPDATE `areatrigger_teleport` SET
+        `target_position_x` = 3817.05,
+        `target_position_y` = 2032.99,
+        `target_position_z` = 10.96,
+        `target_orientation` = 3.6828
+WHERE `id` = 5113;
---
+++ wotlk/areatrigger/5235_Ahn'Kahet_exit.sql
@@ -0,0 +1,6 @@
+UPDATE `areatrigger_teleport` SET
+	`target_position_x` = 3801.4895,
+	`target_position_y` = 2010.4725,
+	`target_position_z` = 9.8038,
+	`target_orientation` = 1.5661
+WHERE `id` = 5235;
