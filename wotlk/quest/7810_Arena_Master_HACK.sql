34161befb7cb9b9eeabf5cdd7f09617976f69ebc
Eugene <codels25@gmail.com>
2011-05-20 12:46:52 +0400

    change quest for arena gurubashi... visible item in loot

---
+++ wotlk/quest/7810_Arena_Master_HACK.sql
@@ -0,0 +1,4 @@
+UPDATE `quest_template` SET
+	`NextQuestId` = 7838,
+	`NextQuestInChain` = 7838
+WHERE `entry` = 7810;
---
+++ wotlk/quest/7838_Arena_Grandmaster_HACK.sql
@@ -0,0 +1,6 @@
+UPDATE `quest_template` SET
+	`Method` = 2,
+	`ZoneOrSort` = 33,
+	`MinLevel` = 1,
+	`Type` = 41
+WHERE `entry` = 7838;
