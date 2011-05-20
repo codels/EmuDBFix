14015c5805b327bcb125887a544b0ffdeddefafd
Eugene <codels25@gmail.com>
2011-05-20 12:47:46 +0400

    add hack for quest 24451 & 20439

---
+++ wotlk/area/4395_Dalaran_HACK.sql
@@ -0,0 +1,6 @@
+INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_start_active`, `quest_end`,
+`aura_spell`, `racemask`, `gender`, `autocast`) VALUES
+(70971, 4395, 24451, 1, 24451, 0, 690, 1, 1),
+(70972, 4395, 24451, 1, 24451, 0, 690, 0, 1),
+(70973, 4395, 20439, 1, 20439, 0, 1101, 1, 1),
+(70974, 4395, 20439, 1, 20439, 0, 1101, 0, 1);
