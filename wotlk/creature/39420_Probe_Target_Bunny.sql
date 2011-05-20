fc7befa4825c52b9987d1dd887c9e8dc75a662ff
Eugene <codels25@gmail.com>
2011-05-20 21:01:01 +0400

    fix creature 39420

---
+++ wotlk/creature/39420_Probe_Target_Bunny.sql
@@ -0,0 +1,7 @@
+UPDATE `creature_template` SET
+        `modelid1` = 16946,
+        `faction_A` = 35,
+        `faction_H` = 35,
+        `npcflag` = 0,
+        `unit_flags` = 0
+WHERE `entry` = 39420;
