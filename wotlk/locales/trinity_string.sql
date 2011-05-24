8de66f505065e37920ee8baadc23f06400a5f03c
Eugene <codels25@gmail.com>
2011-05-24 18:49:42 +0400

    correct rus text

--- wotlk/locales/trinity_string.sql
+++ wotlk/locales/trinity_string.sql
@@ -1,2 +1,3 @@
 UPDATE `trinity_string` SET `content_loc8` = '$n роняет флаг Орды!' WHERE `entry` = 607;
 UPDATE `trinity_string` SET `content_loc8` = '$n роняет флаг Альянса!' WHERE `entry` = 608;
+UPDATE `trinity_string` SET `content_loc8` = '$n возвращает на базу флаг Альянса!' WHERE `entry` = 609;
