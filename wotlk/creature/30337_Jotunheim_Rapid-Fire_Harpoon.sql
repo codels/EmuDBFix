972b92da1d8221b259a8b1e2d98ce948a52409d3
Eugene <codels25@gmail.com>
2011-05-25 16:04:39 +0400

    add spell for creature 30337

---
+++ wotlk/creature/30337_Jotunheim_Rapid-Fire_Harpoon.sql
@@ -0,0 +1,14 @@
+UPDATE `creature_template` SET
+        `speed_walk` = 0,
+        `speed_run` = 0,
+        `mindmg` = 0,
+        `maxdmg` = 0,
+        `attackpower` = 0,
+        `dmg_multiplier` = 0,
+        `rangeattacktime` = 0,
+        `minrangedmg` = 0,
+        `maxrangedmg` = 0,
+        `rangedattackpower` = 0,
+        `spell1` = 56570,
+        `spell3` = 56585 
+WHERE `entry` = 30337;
