a3a4db1f2453e6ae1e1ae3e7f48089f9c33fea73
Eugene <codels25@gmail.com>
2011-05-25 16:03:01 +0400

    add rus text for creature 15956

--- wotlk/creature/15956_AnubRekhan.sql
+++ wotlk/creature/15956_AnubRekhan.sql
@@ -7,3 +7,17 @@ UPDATE `creature_template` SET
         `dmg_multiplier` = if(`entry` = 15956, 35, 70),
         `rangeattacktime` = 0
 WHERE `entry` IN (15956, 29249);
+
+DELETE FROM `script_texts` WHERE `npc_entry` = 15956 OR `entry` IN (-1533000, -1533001, -1533002, -1533003, -1533004, -1533005, -1533006, -1533007, -1533008);
+INSERT INTO `script_texts` (`npc_entry`, `entry`, `content_default`, `content_loc1`, `content_loc2`,
+`content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`,
+`sound`, `type`, `language`, `emote`, `comment`) VALUES 
+(15956, -1533008, 'Shh... it will all be over soon.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 8789, 1, 0, 0, 'anubrekhan SAY_SLAY'),
+(15956, -1533007, 'Closer now... tasty morsels. I\'ve been too long without food. Without blood to drink.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 8793, 1, 0, 0, 'anubrekhan SAY_TAUNT4'),
+(15956, -1533006, 'Which one shall I eat first? So difficult to choose... the all smell so delicious.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 8792, 1, 0, 0, 'anubrekhan SAY_TAUNT3'),
+(15956, -1533005, 'Where to go? What to do? So many choices that all end in pain, end in death.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 8791, 1, 0, 0, 'anubrekhan SAY_TAUNT2'),
+(15956, -1533004, 'I hear little hearts beating. Yesss... beating faster now. Soon the beating will stop.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Я слышу биение маленьких сердец. Да... теперь они бьются быстрей. Но скоро они остановятся.', 8790, 1, 0, 0, 'anubrekhan SAY_TAUNT1'),
+(15956, -1533003, 'Yes, Run! It makes the blood pump faster!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 8787, 1, 0, 0, 'anubrekhan SAY_AGGRO3'),
+(15956, -1533002, 'There is no way out.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Вам отсюда не выбраться.', 8786, 1, 0, 0, 'anubrekhan SAY_AGGRO2'),
+(15956, -1533000, 'Ahh... welcome to my parlor.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 8788, 1, 0, 0, 'anubrekhan SAY_GREET'),
+(15956, -1533001, 'Just a little taste...', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 8785, 1, 0, 0, 'anubrekhan SAY_AGGRO1');
