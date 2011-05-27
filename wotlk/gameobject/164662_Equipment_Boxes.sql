a9011268953a35c0173794627011822f71850015
Eugene <codels25@gmail.com>
2011-05-27 21:16:40 +0400

    style...

--- wotlk/gameobject/164662_Equipment_Boxes.sql
+++ wotlk/gameobject/164662_Equipment_Boxes.sql
@@ -1,7 +1,8 @@
+-- 164662 ytdb loot
 UPDATE `gameobject_template` SET
 	`data1` = 10984
 WHERE `entry` = 164662;
 
-DELETE FROM `gameobject_loot_template` WHERE `entry` IN (164662, 10984); -- 164662 ytdb loot
+DELETE FROM `gameobject_loot_template` WHERE `entry` IN (164662, 10984);
 INSERT INTO `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
 (10984, 11127, -100, 1, 0, 1, 1);
