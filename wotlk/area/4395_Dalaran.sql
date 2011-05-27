37a69fe2cbbd0dfa1f85bfe254f8ce12f2532597
Eugene <codels25@gmail.com>
2011-05-27 21:17:34 +0400

    style...

--- wotlk/area/4395_Dalaran.sql
+++ wotlk/area/4395_Dalaran.sql
@@ -1,7 +1,6 @@
 -- Hack
 DELETE FROM `spell_area` WHERE `spell` BETWEEN 70971 AND 70974;
-INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_start_active`, `quest_end`,
-`aura_spell`, `racemask`, `gender`, `autocast`) VALUES
+INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_start_active`, `quest_end`, `aura_spell`, `racemask`, `gender`, `autocast`) VALUES
 (70971, 4395, 24451, 1, 24451, 0, 690, 1, 1),
 (70972, 4395, 24451, 1, 24451, 0, 690, 0, 1),
 (70973, 4395, 20439, 1, 20439, 0, 1101, 1, 1),
