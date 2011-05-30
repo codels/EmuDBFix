cabdf8315481e96b53029fa4875e57594fe73d7d
Eugene <codels25@gmail.com>
2011-05-30 12:43:59 +0400

    [SQL] Fixed querys for creature 11949.

--- wotlk/creature/11949_Captain_Balinda_Stonehearth.sql
+++ wotlk/creature/11949_Captain_Balinda_Stonehearth.sql
@@ -9,5 +9,5 @@ DELETE FROM `script_texts` WHERE `npc_entry` = 11949 OR `entry` IN (-1810023, -1
 INSERT INTO `script_texts` (`npc_entry`, `entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`,
 `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`, `sound`, `type`, `language`,
 `emote`, `comment`) VALUES
-(11949, -1810023, 'Begone, uncouth scum! The Alliance shall prevail in Alterac Valley!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Прочь, подлецы! Альтеракская долина достанется Альянсу!', 0, 1, 0, 0, NULL);
+(11949, -1810023, 'Begone, uncouth scum! The Alliance shall prevail in Alterac Valley!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Прочь, подлецы! Альтеракская долина достанется Альянсу!', 0, 1, 0, 0, NULL),
 (11949, -1810024, 'Filthy Frostwolf cowards! If you want a fight, you\'ll have to come to me!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Эй вы, жалкие трусы из Северного Волка! Хотите драться – так идите ко мне!', 0, 1, 0, 0, NULL);
