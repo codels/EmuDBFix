c9d4f9160ad492a077fce37552ec2c76a550029f
Eugene <codels25@gmail.com>
2011-06-01 16:39:10 +0400

    [DEV] Clear...

---
+++ wotlk/develop/Fix.sql
@@ -0,0 +1,4 @@
+-- deleted unsed script
+DELETE FROM `gossip_scripts` WHERE `id` = 1021101;
+DELETE FROM `gossip_menu_option` WHERE `menu_id` = 10211 AND `id` = 1;
+UPDATE `quest_template` SET `RewItemCount1` = 0 WHERE `RewItemId1` = 0;
--- wotlk/develop/quest_template.sql
+++
@@ -1,1 +0,0 @@
-UPDATE `quest_template` SET `RewItemCount1` = 0 WHERE `RewItemId1` = 0;
