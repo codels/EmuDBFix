8800ab0c4b7b048b55d3112bfb4804e4065d1d43
Eugene <codels25@gmail.com>
2011-06-01 18:02:18 +0400

    [DEV] Fixed damage for creature #36612.

---
+++ wotlk/develop/36612_Lord_Marrowgar.sql
@@ -0,0 +1,5 @@
+-- old value :35, this is not blizzlike
+UPDATE `creature_template` SET `dmg_multiplier` = 75, `baseattacktime` = 1000 WHERE `entry` = 36612;
+UPDATE `creature_template` SET `dmg_multiplier` = 85 WHERE `entry` = 37957;
+UPDATE `creature_template` SET `dmg_multiplier` = 95 WHERE `entry` = 37958;
+UPDATE `creature_template` SET `dmg_multiplier` = 105 WHERE `entry` = 37959;
