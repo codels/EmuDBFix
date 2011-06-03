bf515ec158951a0df20d2f83390ae5c6d4c2e9ae
Eugene <codels25@gmail.com>
2011-06-03 13:13:25 +0400

    [DEV] Fixed type text for creature #6

--- wotlk/develop/00006_Kobold_Vermin.sql
+++ wotlk/develop/00006_Kobold_Vermin.sql
@@ -7,9 +7,9 @@ DELETE FROM `smart_scripts` WHERE `entryorguid` = @NPC AND `source_type` = 0;
 INSERT INTO `smart_scripts` (`entryorguid`, `event_type`, `action_type`, `target_type`, `comment`) VALUES (@NPC, 4, 1, 2, 'Say on aggro');
 
 DELETE FROM `creature_text` WHERE `entry` = @NPC;
-INSERT INTO `creature_text` (`entry`, `id`, `text`, `comment`) VALUES
-(@NPC, 0, 'You no take candle!', 'Kobold Vermin'),
-(@NPC, 1, 'Yiieeeee! Me run!', 'Kobold Vermin');
+INSERT INTO `creature_text` (`entry`, `id`, `text`, `type`, `comment`) VALUES
+(@NPC, 0, 'You no take candle!', 12, 'Kobold Vermin'),
+(@NPC, 1, 'Yiieeeee! Me run!', 12, 'Kobold Vermin');
 
 /*
 russian:
