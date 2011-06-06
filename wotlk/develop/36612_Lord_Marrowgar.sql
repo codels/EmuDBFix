64a36bd6f69ad7f2283a70a6d455a069a57fd3fe
Eugene <codels25@gmail.com>
2011-06-06 14:20:09 +0400

    [DEV] Correct damage for creature 36612

--- wotlk/develop/36612_Lord_Marrowgar.sql
+++ wotlk/develop/36612_Lord_Marrowgar.sql
@@ -1,5 +1,8 @@
--- old value :35, this is not blizzlike
-UPDATE `creature_template` SET `dmg_multiplier` = 75, `baseattacktime` = 1000 WHERE `entry` = 36612;
-UPDATE `creature_template` SET `dmg_multiplier` = 85 WHERE `entry` = 37957;
-UPDATE `creature_template` SET `dmg_multiplier` = 95 WHERE `entry` = 37958;
-UPDATE `creature_template` SET `dmg_multiplier` = 105 WHERE `entry` = 37959;
+-- 10N
+UPDATE `creature_template` SET `dmg_multiplier` = 75, `baseattacktime` = 1500 WHERE `entry` = 36612;
+-- 25N
+UPDATE `creature_template` SET `dmg_multiplier` = 95, `baseattacktime` = 1500 WHERE `entry` = 37957;
+-- 10H
+UPDATE `creature_template` SET `dmg_multiplier` = 85, `baseattacktime` = 1500 WHERE `entry` = 37958;
+-- 25H
+UPDATE `creature_template` SET `dmg_multiplier` = 105, `baseattacktime` = 1500 WHERE `entry` = 37959;
