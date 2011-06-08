f8d8bfe2c28fe459a225fb035d41e1bf97f43244
Eugene <codels25@gmail.com>
2011-06-08 12:47:29 +0400

    [DEV] Added coord for spell #28444.

---
+++ wotlk/develop/28444_spell.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_target_position` WHERE `id` = 28444;
+INSERT INTO `spell_target_position` (`id`, `target_map`, `target_position_x`, `target_position_y`, `target_position_z`, `target_orientation`) VALUES
+(28444, 533, 3005.68, -3447.77, 293.93, 4.65);
