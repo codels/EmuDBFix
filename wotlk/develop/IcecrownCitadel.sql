d60b278446e398578e59ad23c7556d2331763aac
Eugene <codels25@gmail.com>
2011-06-09 14:09:23 +0400

    Dev: Added SAI for creature 37023

--- wotlk/develop/IcecrownCitadel.sql
+++ wotlk/develop/IcecrownCitadel.sql
@@ -204,6 +204,25 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (@NPC, 0, 2, 0, 9, 0, 100, 0, 8, 40, 1500, 3000, 11, 71140, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Blighted Abomination - Cast Scourge Hook');
 
 /*#####################################
+####       Plague Scientist
+#####################################*/
+
+SET @NPC = 37023;
+
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = @NPC;
+
+DELETE FROM `creature_ai_scripts` WHERE `creature_id` = @NPC;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid` = @NPC AND `source_type` = 0;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`,
+`event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`,
+`action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`,
+`target_z`, `target_o`, `comment`) VALUES
+(@NPC, 0, 0, 0, 0, 0, 100, 0, 5000, 10000, 10000, 15000, 11, 71103, 1, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 'Plague Scientist - Cast Combobulating Spray'),
+(@NPC, 0, 1, 0, 0, 0, 100, 0, 500, 1500, 2600, 2700, 11, 73079, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Plague Scientist - Cast Plague Blast'),
+(@NPC, 0, 2, 0, 0, 0, 100, 0, 15000, 20000, 15000, 20000, 11, 69871, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Plague Scientist - Cast Plague Stream');
+
+/*#####################################
 ####     Lord Marrowgar
 #####################################*/
 
