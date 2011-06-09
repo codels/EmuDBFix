37d68ffbfd8cc1df8e313dcd9c95504aeafabca4
Eugene <codels25@gmail.com>
2011-06-09 14:00:32 +0400

    Dev: Added SAI for creature 37022

--- wotlk/develop/IcecrownCitadel.sql
+++ wotlk/develop/IcecrownCitadel.sql
@@ -185,6 +185,25 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (@NPC, 0, 0, 0, 0, 0, 100, 0, 3000, 5000, 7500, 10000, 11, 70964, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Ancient Skeletal Soldier - Cast Shield Bash');
 
 /*#####################################
+####      Blighted Abomination
+#####################################*/
+
+SET @NPC = 37022;
+
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = @NPC;
+
+DELETE FROM `creature_ai_scripts` WHERE `creature_id` = @NPC;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid` = @NPC AND `source_type` = 0;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`,
+`event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`,
+`action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`,
+`target_z`, `target_o`, `comment`) VALUES
+(@NPC, 0, 0, 0, 0, 0, 100, 0, 500, 1500, 1500, 2000, 11, 40504, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Blighted Abomination - Cast Cleave'),
+(@NPC, 0, 1, 0, 0, 0, 100, 0, 3500, 5000, 15000, 20000, 11, 71150, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Blighted Abomination - Cast Plague Cloud'),
+(@NPC, 0, 2, 0, 9, 0, 100, 0, 8, 40, 1500, 3000, 11, 71140, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Blighted Abomination - Cast Scourge Hook');
+
+/*#####################################
 ####     Lord Marrowgar
 #####################################*/
 
