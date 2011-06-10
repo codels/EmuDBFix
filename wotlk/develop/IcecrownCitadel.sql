db1342f1ee58dabc3b4a56d4c7fc954f458c2d33
Eugene <codels25@gmail.com>
2011-06-10 12:48:49 +0400

    Dev: Added SAI for creature 10404

--- wotlk/develop/IcecrownCitadel.sql
+++ wotlk/develop/IcecrownCitadel.sql
@@ -1,6 +1,25 @@
 -- spell 71203 - in kernel.
 
 /*#####################################
+####      Pustulating Horror
+#####################################*/
+
+SET @NPC = 10404;
+
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = @NPC;
+
+DELETE FROM `creature_ai_scripts` WHERE `creature_id` = @NPC;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid` = @NPC AND `source_type` = 0;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`,
+`event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`,
+`action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`,
+`target_z`, `target_o`, `comment`) VALUES
+(@NPC, 0, 0, 0, 2, 0, 100, 0, 0, 10, 5100, 5200, 11, 71088, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Pustulating Horror - Cast Blight Bomb'),
+(@NPC, 0, 1, 0, 0, 0, 100, 10, 1500, 3000, 5500, 7000, 11, 71089, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 'Pustulating Horror - Cast Bubbling Pus (10)'),
+(@NPC, 0, 2, 0, 0, 0, 100, 20, 1500, 3000, 5500, 7000, 11, 71090, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 'Pustulating Horror - Cast Bubbling Pus (25)');
+
+/*#####################################
 ####     Servant of the Throne
 #####################################*/
 
