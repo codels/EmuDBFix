f13425867c257ce16b9f281da5951b29fc53e742
Eugene <codels25@gmail.com>
2011-06-10 13:18:06 +0400

    Dev: Added SAI for creature 36880

--- wotlk/develop/IcecrownCitadel.sql
+++ wotlk/develop/IcecrownCitadel.sql
@@ -151,6 +151,24 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (@NPC, 0, 1, 0, 0, 0, 100, 0, 1500, 3000, 8500, 11000, 11, 69483, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 'Deathspeaker High Priest - Cast Dark Reckoning');
 
 /*#####################################
+####      Decaying Colossus
+#####################################*/
+
+SET @NPC = 36880;
+
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = @NPC;
+
+DELETE FROM `creature_ai_scripts` WHERE `creature_id` = @NPC;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid` = @NPC AND `source_type` = 0;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`,
+`event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`,
+`action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`,
+`target_z`, `target_o`, `comment`) VALUES
+(@NPC, 0, 0, 0, 0, 0, 100, 10, 5000, 8000, 5000, 8000, 11, 71114, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Decaying Colossus - Cast Massive Stomp (10)'),
+(@NPC, 0, 1, 0, 0, 0, 100, 20, 5000, 8000, 5000, 8000, 11, 71115, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Decaying Colossus - Cast Massive Stomp (25)');
+
+/*#####################################
 ####     Deathbound Ward
 #####################################*/
 
