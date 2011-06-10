1db216bccfc75b01f459e68f0758c7c9f3d38e38
Eugene <codels25@gmail.com>
2011-06-10 13:57:49 +0400

    TDB/YTDB: Changed SAI for creature 28138 (need test)

--- wotlk/creature/28138_Elder_Harkek.sql
+++ wotlk/creature/28138_Elder_Harkek.sql
@@ -19,10 +19,28 @@ INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry
 
 DELETE FROM `gossip_scripts` WHERE `id` IN (28138, 28139, 28140);
 
+DELETE FROM `creature_ai_scripts` WHERE `creature_id` = @NPC;
+
 DELETE FROM `smart_scripts` WHERE `entryorguid` = @NPC AND `source_type` = 0;
-INSERT INTO `smart_scripts` (`entryorguid`, `id`, `link`, `event_type`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `target_type`, `comment`) VALUES
-(@NPC, 0, 4, 62, @GOSSIP, 0, 0, 0, 85, 52544, 7, 'Invoker self cast'),
-(@NPC, 1, 4, 62, @GOSSIP, 1, 0, 0, 85, 52544, 7, 'Invoker self cast'),
-(@NPC, 2, 4, 62, @GOSSIP, 2, 0, 0, 85, 52542, 7, 'Invoker self cast'),
-(@NPC, 3, 4, 62, @GOSSIP, 3, 0, 0, 85, 52545, 7, 'Invoker self cast'),
-(@NPC, 4, 0, 61, 0, 0, 0, 0, 72, 0, 7, 'Close gossip');
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`,
+`event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`,
+`action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`,
+`target_z`, `target_o`, `comment`) VALUES
+(@NPC, 0, 0, 4, 62, 0, 100, 0, @GOSSIP, 0, 0, 0, 85, 52544, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Elder Harkek - Player self cast'),
+(@NPC, 0, 1, 4, 62, 0, 100, 0, @GOSSIP, 1, 0, 0, 85, 52544, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Elder Harkek - Player self cast'),
+(@NPC, 0, 2, 4, 62, 0, 100, 0, @GOSSIP, 2, 0, 0, 85, 52542, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Elder Harkek - Player self cast'),
+(@NPC, 0, 3, 4, 62, 0, 100, 0, @GOSSIP, 3, 0, 0, 85, 52545, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Elder Harkek - Player self cast'),
+(@NPC, 0, 4, 0, 61, 0, 100, 0, 0, 0, 0, 0, 72, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Elder Harkek - Close gossip'),
+(@NPC, 0, 5, 6, 25, 0, 100, 0, 0, 0, 0, 0, 21, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Elder Harkek - Prevent Combat Movement on Reset'),
+(@NPC, 0, 6, 0, 61, 0, 100, 0, 0, 0, 0, 0, 22, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Elder Harkek - Set Phase to 0 on Reset'),
+(@NPC, 0, 7, 8, 4, 0, 100, 0, 0, 0, 0, 0, 11, 33643, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Elder Harkek - Cast Chain Lightning on Aggro'),
+(@NPC, 0, 8, 0, 61, 0, 100, 0, 0, 0, 0, 0, 23, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Elder Harkek - Set Phase 1 on Aggro'),
+(@NPC, 0, 9, 0, 9, 1, 100, 0, 0, 40, 6000, 9000, 11, 33643, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Elder Harkek - Cast Chain Lightning (Phase 1)'),
+(@NPC, 0, 10, 10, 3, 1, 100, 0, 0, 0, 7, 0, 21, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Elder Harkek - Start Combat Movement when Mana is at 7% (Phase 1)'),
+(@NPC, 0, 11, 0, 61, 0, 100, 0, 0, 0, 0, 0, 23, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Elder Harkek - Set Phase 2 when Mana is at 7% (Phase 1)'),
+(@NPC, 0, 12, 0, 9, 1, 100, 0, 35, 80, 1000, 1000, 21, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Elder Harkek - Start Combat Movement at 35 Yards (Phase 1)'),
+(@NPC, 0, 13, 0, 9, 1, 100, 0, 5, 15, 1000, 1000, 21, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Elder Harkek - Prevent Combat Movement at 15 Yards (Phase 1)'),
+(@NPC, 0, 14, 0, 9, 1, 100, 0, 0, 5, 1000, 1000, 21, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Elder Harkek - Start Combat Movement Below 5 Yards (Phase 1)'),
+(@NPC, 0, 15, 0, 3, 2, 100, 0, 15, 100, 1000, 1000, 23, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Elder Harkek - Set Phase 1 when Mana is above 15% (Phase 2)'),
+(@NPC, 0, 16, 0, 0, 0, 100, 0, 13000, 18000, 13000, 18000, 11, 52905, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Elder Harkek - Cast Thunderbolt'),
+(@NPC, 0, 17, 0, 7, 0, 100, 0, 0, 0, 0, 0, 22, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Elder Harkek - Set Phase to 0 on Evade');
