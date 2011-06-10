dbc916c143909ae1604eec143442c9216d40d444
Eugene <codels25@gmail.com>
2011-06-10 13:36:26 +0400

    Dev: Added SAI for creature 37098

--- wotlk/develop/IcecrownCitadel.sql
+++ wotlk/develop/IcecrownCitadel.sql
@@ -287,6 +287,24 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (@NPC, 0, 0, 0, 9, 0, 100, 0, 5, 30, 6000, 8000, 11, 71164, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Vengeful Fleshreaper - Cast Leaping Face Maul');
 
 /*#####################################
+####         Val'kyr Herald
+#####################################*/
+
+SET @NPC = 37098;
+
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = @NPC;
+
+DELETE FROM `creature_ai_scripts` WHERE `creature_id` = @NPC;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid` = @NPC AND `source_type` = 0;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`,
+`event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`,
+`action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`,
+`target_z`, `target_o`, `comment`) VALUES
+(@NPC, 0, 0, 0, 0, 0, 100, 10, 2500, 4000, 2500, 4000, 11, 71906, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 'Val\'kyr Herald - Cast Severed Essence (10)'),
+(@NPC, 0, 1, 0, 0, 0, 100, 20, 2500, 4000, 2500, 4000, 11, 71942, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 'Val\'kyr Herald - Cast Severed Essence (25)');
+
+/*#####################################
 ####           Precious
 #####################################*/
 
