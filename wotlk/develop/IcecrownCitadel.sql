65d1e55ef6d2356033972dde14d8ab81e1fa1f68
Eugene <codels25@gmail.com>
2011-06-15 13:52:38 +0400

    Dev: Added SAI for creature 37595.

--- wotlk/develop/IcecrownCitadel.sql
+++ wotlk/develop/IcecrownCitadel.sql
@@ -315,6 +315,27 @@ DELETE FROM `creature_ai_scripts` WHERE `creature_id` = @NPC;
 DELETE FROM `smart_scripts` WHERE `entryorguid` = @NPC AND `source_type` = 0;
 
 /*#####################################
+####    Darkfallen Blood Knight
+#####################################*/
+
+SET @NPC = 37595;
+
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = @NPC;
+
+DELETE FROM `creature_ai_scripts` WHERE `creature_id` = @NPC;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid` = @NPC AND `source_type` = 0;
+
+-- Spell [70450] Blood Mirror (need?)
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`,
+`event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`,
+`action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`,
+`target_z`, `target_o`, `comment`) VALUES
+(@NPC, 0, 0, 0, 0, 0, 100, 0, 6000, 7000, 30000, 40000, 11, 70299, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 'Darkfallen Blood Knight - Cast Siphon Essence'),
+(@NPC, 0, 1, 0, 4, 0, 100, 0, 0, 0, 0, 0, 11, 71736, 3, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Darkfallen Blood Knight - Cast Vampiric Aura'),
+(@NPC, 0, 2, 0, 0, 0, 100, 0, 500, 1000, 1500, 1600, 11, 70437, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Darkfallen Blood Knight - Cast Unholy Strike');
+
+/*#####################################
 ####        Darkfallen Noble
 #####################################*/
 
