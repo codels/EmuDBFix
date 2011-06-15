3f2c9b75a7e0cc455399ebb2e31b0550ddf26145
Eugene <codels25@gmail.com>
2011-06-15 13:31:36 +0400

    Dev: Added SAI for creature 37663.

--- wotlk/develop/IcecrownCitadel.sql
+++ wotlk/develop/IcecrownCitadel.sql
@@ -315,6 +315,26 @@ DELETE FROM `creature_ai_scripts` WHERE `creature_id` = @NPC;
 DELETE FROM `smart_scripts` WHERE `entryorguid` = @NPC AND `source_type` = 0;
 
 /*#####################################
+####        Darkfallen Noble
+#####################################*/
+
+SET @NPC = 37663;
+
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = @NPC;
+
+DELETE FROM `creature_ai_scripts` WHERE `creature_id` = @NPC;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid` = @NPC AND `source_type` = 0;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`,
+`event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`,
+`action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`,
+`target_z`, `target_o`, `comment`) VALUES
+(@NPC, 0, 0, 0, 0, 0, 100, 0, 6000, 7000, 30000, 40000, 11, 70299, 1, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 'Darkfallen Noble - Cast Siphon Essence'),
+(@NPC, 0, 1, 0, 0, 0, 100, 0, 3500, 5000, 9000, 11000, 11, 70645, 1, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 'Darkfallen Noble - Cast Chains of Shadow'),
+(@NPC, 0, 2, 0, 0, 0, 100, 10, 500, 1500, 2100, 2200, 11, 72960, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Darkfallen Noble - Cast Shadow Bolt (10)'),
+(@NPC, 0, 3, 0, 0, 0, 100, 20, 500, 1500, 2100, 2200, 11, 72961, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Darkfallen Noble - Cast Shadow Bolt (25)');
+
+/*#####################################
 ####       Darkfallen Archmage
 #####################################*/
 
