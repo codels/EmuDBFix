c7d9643ddf284afc6e23dcd5e1c9c1739fe4031c
Eugene <codels25@gmail.com>
2011-06-15 16:51:57 +0400

    Dev: Added SAI for creature 37133. (need test)

--- wotlk/develop/IcecrownCitadel.sql
+++ wotlk/develop/IcecrownCitadel.sql
@@ -310,9 +310,16 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 
 SET @NPC = 37133;
 
-UPDATE `creature_template` SET `AIName` = '', `ScriptName` = 'npc_frostwing_vrykul' WHERE `entry` = @NPC;
+UPDATE `creature_template` SET `AIName` = 'SmartAI', `ScriptName` = 'npc_frostwing_vrykul' WHERE `entry` = @NPC;
+
 DELETE FROM `creature_ai_scripts` WHERE `creature_id` = @NPC;
+
 DELETE FROM `smart_scripts` WHERE `entryorguid` = @NPC AND `source_type` = 0;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`,
+`event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`,
+`action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`,
+`target_z`, `target_o`, `comment`) VALUES
+(@NPC, 0, 0, 0, 9, 0, 100, 0, 0, 10, 2100, 2200, 11, 41056, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Ymirjar Warlord - Cast Whirlwind');
 
 /*#####################################
 ####        Ymirjar Huntress
