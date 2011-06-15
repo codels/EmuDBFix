f603ec0bdeae562002388099fd180f9c7a5f0ab5
Eugene <codels25@gmail.com>
2011-06-15 14:11:32 +0400

    Dev: Added SAI for creature 37662.

--- wotlk/develop/IcecrownCitadel.sql
+++ wotlk/develop/IcecrownCitadel.sql
@@ -356,6 +356,25 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (@NPC, 0, 2, 0, 0, 0, 100, 0, 500, 1000, 1500, 1600, 11, 70437, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Darkfallen Blood Knight - Cast Unholy Strike');
 
 /*#####################################
+####      Darkfallen Commander
+#####################################*/
+
+SET @NPC = 37662;
+
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = @NPC;
+
+DELETE FROM `creature_ai_scripts` WHERE `creature_id` = @NPC;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid` = @NPC AND `source_type` = 0;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`,
+`event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`,
+`action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`,
+`target_z`, `target_o`, `comment`) VALUES
+(@NPC, 0, 0, 0, 0, 0, 100, 0, 1000, 2000, 30000, 31000, 11, 70750, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Darkfallen Commander - Cast Battle Shout'),
+(@NPC, 0, 1, 0, 9, 0, 100, 10, 10, 30, 3000, 4000, 11, 70449, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Darkfallen Commander - Cast Vampire Rush (10)'),
+(@NPC, 0, 2, 0, 9, 0, 100, 20, 10, 30, 3000, 4000, 11, 71155, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Darkfallen Commander - Cast Vampire Rush (25)');
+
+/*#####################################
 ####        Darkfallen Noble
 #####################################*/
 
